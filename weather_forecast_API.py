2233f8f90256c97dcd0257c8e5b5e5327bdf34b2
bsao17 <greta.b.meddhed>
2020-11-03 19:16:45 +0100

    Init paragliding weather station for micro:bit

---
+++ weather_forecast_API.py
@@ -0,0 +1,12 @@
+import pyowm
+
+## Copy and paste your key into the line below
+KEY = '1234567890abcdefghijklmnopqrstuvwxyz'
+
+## Place your location (city, country code) into the line below
+location = 'New York,us'
+
+owm = pyowm.OWM(KEY)
+fc = owm.three_hours_forecast(location)
+f = fc.get_forecast()
+icons = [weather.get_weather_icon_name() for weather in f]
